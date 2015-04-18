10834af483269c406211b3919fd4eefdc3f34ece
Pedro Duran <pedro.duranc@gmail.com>
2015-04-18 11:06:15 +0200

    First refactor

--- app/src/androidTest/java/com/peshe/coding_dojo/codingdojo/models/BasketTest.java
+++ app/src/androidTest/java/com/peshe/coding_dojo/codingdojo/models/BasketTest.java
@@ -7,21 +7,25 @@ import android.test.InstrumentationTestCase;
  */
 public class BasketTest extends InstrumentationTestCase {
 
-    
+    public static double getPrice(int bookNumber) {
+        switch (bookNumber) {
+            case 1:
+                return 8;
+            case 2:
+                return 2 * 8 * 0.95;
+        }
+        return 0;
+    }
 
     public void test1BookIs8Eur() throws Exception {
-        int precio = 8;
-
-        assertEquals(precio, 8);
+        assertEquals(getPrice(1), 8.0);
     }
 
     public void test1BookType1and1BookType2HasDiscount() throws Exception {
-        double precioType1 = 0;
-
+        double precioType1 = 8;
         double precioType2 = 8;
-        double precioTotal = (precioType1 + precioType2) * 0.95;
         double precioTotalWithDiscount = (precioType1 + precioType2) * 0.95;
 
-        assertEquals(precioTotal, precioTotalWithDiscount);
+        assertEquals(getPrice(2), precioTotalWithDiscount);
     }
 }
